0fab4b98dac4133f190490fa71828bf85ed10e1a
junmindereal <junmindereal@gmail.com>
2023-03-07 16:01:12 +0800

    updated tab stops

--- lua/junmin/set.lua
+++ lua/junmin/set.lua
@@ -5,9 +5,9 @@ opt.nu = true
 opt.relativenumber = true
 
 -- tabs and indentation
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 opt.smartindent = true
