22faab839a6e6cac2e285c0023792dd5196be51a
junmindereal <junmindereal@gmail.com>
2023-06-12 10:12:38 +0800

    Dont format on save

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -26,23 +26,25 @@ null_ls.setup({
 			end,
 		}),
 	},
-	-- configure format on save
-	on_attach = function(current_client, bufnr)
-		if current_client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						filter = function(client)
-							--  only use null-ls for formatting instead of lsp server
-							return client.name == "null-ls"
-						end,
-						bufnr = bufnr,
-					})
-				end,
-			})
-		end
-	end,
+
+	-- -- configure format on save
+	-- on_attach = function(current_client, bufnr)
+	-- 	if current_client.supports_method("textDocument/formatting") then
+	-- 		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	-- 		vim.api.nvim_create_autocmd("BufWritePre", {
+	-- 			group = augroup,
+	-- 			buffer = bufnr,
+	-- 			callback = function()
+	-- 				vim.lsp.buf.format({
+	-- 					filter = function(client)
+	-- 						--  only use null-ls for formatting instead of lsp server
+	-- 						return client.name == "null-ls"
+	-- 					end,
+	-- 					bufnr = bufnr,
+	-- 				})
+	-- 			end,
+	-- 		})
+	-- 	end
+	-- end,
+	--
 })
